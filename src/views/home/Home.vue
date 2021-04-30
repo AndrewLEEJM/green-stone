3963b024ea8ddb9187ded0ea40eb2154582be479
AndrewLEEJM <woals214@gmail.com>
2021-04-30 17:46:30 +0900

    이미지 적용

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="mainContainer">사진</div>
+    <div class="mainContainer"></div>
     <div class="businessSection">
       <h2 align="center">B U S I N E S S</h2>
       <ul>
@@ -36,19 +36,22 @@ body {
 }
 .mainContainer {
   width: 100%;
-  height: 400px;
+  height: 500px;
   background-color: antiquewhite;
+  background-image: url('../../assets/images/factory.jpg');
 }
 .businessSection {
   width: 100%;
   height: 400px;
   background-color: rgb(218, 157, 78);
+  background-image: url('../../assets/images/stone.jpg');
 }
 .businessSection h2 {
   margin: 0;
   margin-left: 35px;
   font-size: 1.875rem;
   padding-top: 70px;
+  color: rgb(240, 240, 240);
 }
 .businessSection ul {
   list-style: none;
@@ -56,8 +59,11 @@ body {
   justify-content: space-evenly;
   width: 80%;
   margin: 0 auto;
-  height: 280px;
+  height: 290px;
   align-items: center;
+}
+.businessSection ul li{
+  color: rgb(240, 240, 240);;
 }
 .businessSection ul li:hover {
   color: rgb(151, 35, 35);
