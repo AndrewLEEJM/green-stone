0be571a6cf8a7f78bec841eac2e3b8bf181a2489
AndrewLEEJM <woals214@gmail.com>
2021-05-12 13:23:10 +0900

    반응형 css

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,21 @@
     <router-view />
     <footer class="footer">
       <div class="footer_info">
-        <p><span>(주)그린석재</span>
-        <span class="right_side">사업자등록번호: 127-86-02350</span>
-        <span class="right_side">대표: 임만혁</span></p>
+        <p>
+          <span>(주)그린석재</span>
+          <span class="right_side">사업자등록번호: 127-86-02350</span>
+          <span class="right_side">대표: 임만혁</span>
+        </p>
         <span>경기 포천시 일동면 청지로 45 (우)11118</span>
-        <p><span>대표전화: 031) 536-9330~1</span> <span class="right_side">팩스: 031) 536-9332</span></p>
-        <br/>
+        <p>
+          <span>대표전화: 031) 536-9330~1</span>
+          <span class="right_side">팩스: 031) 536-9332</span>
+        </p>
+        <br />
         COPYRIGHT(C) 2021 GREEN STONE. ALL RIGHT RESERVED
       </div>
       <div class="footer_logo">
-        <img src="@/assets/images/logo.png" alt="">
+        <img src="@/assets/images/logo.png" alt="" />
       </div>
     </footer>
   </div>
@@ -62,7 +67,7 @@ export default class App extends Vue {}
   align-items: center;
   align-self: center;
   color: rgb(243, 243, 242);
-  width: 20%;
+  width: 15rem;
 }
 .menu ul {
   color: rgb(243, 243, 242);
@@ -72,33 +77,44 @@ export default class App extends Vue {}
   height: 100%;
   list-style: none;
 }
-.menu ul li{
+.menu ul li {
   margin-left: 50px;
   margin-right: 50px;
 }
-.footer{
+.footer {
   height: 200px;
   display: flex;
   background-color: #0a5d48;
   color: rgb(243, 243, 242);
-  justify-content: space-around
+  justify-content: space-around;
 }
-.footer_info{
+.footer_info {
   width: 500px;
   text-align: left;
 }
-.footer_logo{
+.footer_logo {
   width: 300px;
   align-self: center;
 }
-.footer_logo img{
+.footer_logo img {
   width: 150px;
 }
-.right_side{
+.right_side {
   margin-left: 20px;
 }
-a{
+a {
   text-decoration: none;
   color: rgb(243, 243, 242);
+}
+@media screen and (max-width: 800px) {
+  .menu {
+    display: none;
+  }
+  .footer_logo {
+    display: none;
+  }
+  .footer_info {
+    width: 100%;
+  }
 }
 </style>
